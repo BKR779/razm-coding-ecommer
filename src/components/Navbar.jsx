eb88d6c5a165022b2d841280250257fd0272c617
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-13 23:51:01 +0300

    feat: add basic search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import Nav from './Nav';
+import Search from './Search';
 
 export default function Navbar() {
 	return (
-		<nav className="bg-gray-800">
-			<div className="mx-auto max-w-7x1 px-2 sm:px-6 lg:px-8">
-				<div className="relative flex h-16 items-center justify-center">
-					{/* <div className="absolute inset-y-0 left-0 flex items-center"> */}
-						<Nav name="Home" />
-						<Nav name="Products" />
-						<Nav name="Cart" />
-					{/* </div> */}
+		<>
+			<nav className="bg-gray-800">
+				<div className="mx-auto max-w-7x1 px-2 sm:px-6 lg:px-8">
+					<div className="relative flex h-16 items-center justify-center">
+						{/* <div className="absolute inset-y-0 left-0 flex items-center"> */}
+						<div className="flex flex-1 items-center justify-center">
+							<Nav name="Home" />
+							<Nav name="Products" />
+							<Nav name="Cart" />
+						</div>
+						<div className="justify-end">
+							<Search />
+						</div>
+						{/* </div> */}
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+		</>
 	);
 }
