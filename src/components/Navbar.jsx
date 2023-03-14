0053d3c3075c0be30929d70f32a6e48998636160
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-14 13:17:27 +0300

    fix(navbar): add routing for pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Nav from './Nav';
 import Search from './Search';
+import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Navbar() {
@@ -8,22 +9,28 @@ export default function Navbar() {
 			<nav className="bg-gray-800">
 				<div className="mx-auto max-w-7x1 px-2 sm:px-6 lg:px-8">
 					<div className="relative flex h-16 items-center justify-start">
-						{/* <div className="absolute inset-y-0 left-0 flex items-center"> */}
 						<div className="">
-							<Image
-								src="./vercel.svg"
-								alt="vercel"
-								height="100"
-								width="100"
-							></Image>
+							<Link href="/">
+								<Image
+									src="./vercel.svg"
+									alt="vercel"
+									height={100}
+									width={100}
+								></Image>
+							</Link>
 						</div>
-						<div className="flex flex-1 items-center justify-center">
-							<Nav name="Home" />
-							<Nav name="Products" />
-							<Nav name="Cart" />
+						<div className="flex flex-1 items-center justify-center gap-4">
+							<Link href="/">
+								<Nav name="Home" />
+							</Link>
+							<Link href="/products">
+								<Nav name="Products" />
+							</Link>
+							<Link href="/cart">
+								<Nav name="Cart" />
+							</Link>
 						</div>
 						<Search />
-						{/* </div> */}
 					</div>
 				</div>
 			</nav>
