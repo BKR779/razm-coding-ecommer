0683a4eca8e410eab239d18e0d28729a84b5ce47
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-14 13:17:09 +0300

    feat: add search styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,3 +1,10 @@
 export default function Search() {
-	return <input type="search" name="search" className="" />;
+	return (
+		<input
+			type="search"
+			name="search"
+			className="outline-0 h-8 w-55 border-4 rounded-sm hover:border-blue-300 bg-slate-100"
+			placeholder="Search..."
+		/>
+	);
 }
